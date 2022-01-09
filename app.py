bfbe13563bb09d1c7dc079979d71ff5eade982c2
sahubabu <ashok.pydev@gmail.com>
2022-01-09 16:57:42 +0530

    modified hostpot in feature2

--- app.py
+++ app.py
@@ -49,9 +49,9 @@ def math_operation_via_postman():
 
 
 if __name__ == '__main__':
-    host = "127.0.0.1"
-    port = 8080
-    app.run(host="127.0.0.1", port=8080,debug=True)
+    host = "127.0.011"
+    port = 11111
+    ####app.run(host="127.0.0.1", port=8080,debug=True)
 
 
 
